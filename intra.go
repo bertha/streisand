7636d9ac0573f802500c8c97074822a602918cab
Jille Timmermans <jille@quis.cx>
2021-12-09 11:53:47 +0100

    Methods to push/pull from the remote server
    HTTP endpoints aren't implemented yet though

---
+++ intra.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// pushBlob sends a blob to a target server (given as host:port)
+// Passing in an fh is optional, but if you do, it will be closed before returning.
+func pushBlob(target string, hash []byte, fh *os.File) error {
+	if fh == nil {
+		var err error
+		fh, err = store.Get(hash)
+		if err != nil {
+			return err
+		}
+	}
+	defer fh.Close()
+	st, err := fh.Stat()
+	if err != nil {
+		return err
+	}
+	u := url.URL{
+		Scheme: "http",
+		Host:   target,
+		Path:   "/internal/upload",
+	}
+	req, err := http.NewRequest("POST", u.String(), fh)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Expect", "100-continue")
+	req.Header.Set("X-StreiSANd-Hash", hex.EncodeToString(hash))
+	req.Header.Set("Content-Length", fmt.Sprint(st.Size()))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("HTTP error: %s", resp.Status)
+	}
+	return nil
+}
+
+func pullBlob(target string, hash []byte) error {
+	u := url.URL{
+		Scheme: "http",
+		Host:   target,
+		Path:   "/internal/blob/" + hex.EncodeToString(hash),
+	}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("HTTP error: %s", resp.Status)
+	}
+	w, err := store.NewWriter()
+	if err != nil {
+		return err
+	}
+	defer w.Abort()
+	if _, err := io.Copy(w, resp.Body); err != nil {
+		return err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	if bytes.Compare(hash, w.Hash()) != 0 {
+		return fmt.Errorf("remote returned incorrect file: want %q, got %q", hex.EncodeToString(hash), hex.EncodeToString(w.Hash()))
+	}
+	return nil
+}
